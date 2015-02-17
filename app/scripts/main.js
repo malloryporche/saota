06719c12ca85c92ad6ca5d9fb7b252e79b67b042
Mallory Porche <malloryburke@gmail.com>
2015-02-16 20:39:58 -0800

    Vertical-fullwidth: layout refacotred

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
+// append css dynamically to head
+var head = document.getElementsByTagName('head');
+var element = document.createElement('link');
+element.rel = 'stylesheet';
+element.type = 'text/css';
+element.href = '//www.msmalloryporche.com/folio/tenuto/styles/main.css';
 
-$('body').css('display', 'none');
-
-$('body').fadeIn(1000);}
-
-
-//  QUOTE ANIMATION  //
+//here's the magic
+element.media = 'non-existant-media';
+head.appendChild(element, head.firstChild);
+setTimeout(function () {
+    element.media = 'all';
+});
